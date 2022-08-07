882e648906e7db5ea880ac4bd82c316343aa3d12
Ed Lipski <elipski@gmail.com>
2022-08-07 16:28:01 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//EventHubSender.IEventHubSender ehs = new EventHubSender.EventSender();
+
+await EventHubSender.EventSender.Main();
